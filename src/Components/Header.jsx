4f92da56e8a4c63b2388b53a5df2c1efb6744571
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-08-19 13:11:34 -0400

    added new page
    create desing for header
    modify packaje.json

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,14 @@
 import Navbar from "./Navbar";
 const Header = () => {
     return ( 
-    <div>
-
+    <div id='main'>
+        <Navbar/>
+        <div className='name'>
+            <h1><span>Launch </span>your app with us </h1>
+            <p className='details'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolorem quod laudantium nihil molestiae sequi? Rem magnam nobis sint est placeat quo totam fugit impedit? Maxime fugiat nam cupiditate nemo? 
+                </p>
+            <a href="#" className='cv-btn'>Download</a>
+        </div>
     </div> );
 }
  
