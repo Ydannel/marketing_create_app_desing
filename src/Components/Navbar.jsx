3856a908493fb46dc60d33c0eba62e596fd2c5ad
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-08-19 11:43:56 -0400

    .
    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import Logo from '../images/logo.png'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const changeLogoBack =() =>{
+        if (window.scrollY >= 50) {
+            setNav(true)
+        }
+        else{
+            setNav(false)
+        }
+    }
+    window.addEventListener('scroll', changeLogoBack);
+
     return ( 
-       <nav className={}>
+       <nav className={nav ? 'nav active' : 'nav'}>
         <a href="#" className='logo'>
             <img src={Logo} alt="logo" />
         </a>
@@ -11,7 +24,7 @@ const Navbar = () => {
             <span className='nav-icon' ></span>
         </label>
         <ul className='menu'>
-            <li> <a href="#"> Menu </a> </li>
+            <li> <a href="#" className='active'> Menu </a> </li>
             <li> <a href="#"> Features </a> </li>
             <li> <a href="#"> About </a> </li>
             <li> <a href="#">  UI SS</a> </li>
